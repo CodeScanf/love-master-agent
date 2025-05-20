0221a6eceeb5dcdcc54c431907d2ea18805065a5
CodeScanf <100558509+CodeScanf@users.noreply.github.com>
2025-05-20 19:52:15 +0800

    love-master-agent项目初始化  -xiaoli

---
+++ src/main/java/com/xiaoli/lovemasteragent/LoveMasterAgentApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoli.lovemasteragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveMasterAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveMasterAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoli/lovemasteragent/LoveMasterAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoli.lovemasteragent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveMasterAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
