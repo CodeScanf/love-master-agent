2c4750fbd38e4a45d6ce0284c902b173b7afded0
CodeScanf <100558509+CodeScanf@users.noreply.github.com>
2025-06-16 22:51:31 +0800

    - 实现恋爱 agent 的应用接口开发      - 支持流式调用      - 开发 SSE 流式接口 - 支持跨域 - 测试     -- xiaoli

---
+++ src/main/java/com/xiaoli/lovemasteragent/controller/AiController.java
@@ -0,0 +1,107 @@
+package com.xiaoli.lovemasteragent.controller;
+
+
+import com.xiaoli.lovemasteragent.agent.XiaoliManus;
+import com.xiaoli.lovemasteragent.app.LoveApp;
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/ai")
+public class AiController {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Resource
+    private ToolCallback[] allTools;
+
+    @Resource
+    private ChatModel dashscopeChatModel;
+
+    /**
+     * 同步调用 ai 恋爱大师应用
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    @GetMapping("/love_app/chat/sync")
+    public String doChatWithLoveAppSync(String message, String chatId) {
+        return loveApp.doChat(message, chatId);
+    }
+
+    /**
+     * SSE 流式调用 ai 恋爱大师应用
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    @GetMapping(value = "/love_app/chat/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> doChatWithLoveAppStream(String message, String chatId) {
+        return loveApp.doChatByStream(message, chatId);
+    }
+
+    /**
+     * SSE 流式调用 ai 恋爱大师应用
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    @GetMapping(value = "/love_app/chat/server_sent_event")
+    public Flux<ServerSentEvent<String>> doChatWithLoveAppServerSentEvent(String message, String chatId) {
+        return loveApp.doChatByStream(message, chatId)
+                .map(chunk -> ServerSentEvent.<String>builder()
+                        .data(chunk)
+                        .build());
+    }
+
+    /**
+     * SSE 流式调用 ai 恋爱大师应用
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    @GetMapping(value = "/love_app/chat/sse_emitter")
+    public SseEmitter doChatWithLoveAppServerSseEmitter(String message, String chatId) {
+        SseEmitter sseEmitter = new SseEmitter(180000L);    // 180秒超时
+        // 获取 Flux 响应数据并且直接通过订阅推送给 SSE 订阅者
+        loveApp.doChatByStream(message, chatId)
+                .subscribe(chunk -> {
+                    try {
+                        sseEmitter.send(chunk);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }, sseEmitter::completeWithError, sseEmitter::complete );
+        // 返回 SSE 订阅者
+        return sseEmitter;
+    }
+
+    /**
+     * 流式调用 Manus 超级智能体
+     *
+     * @param message
+     * @return
+     */
+    @GetMapping("/manus/chat")
+    public SseEmitter doChatWithManus(String message) {
+        XiaoliManus xiaoliManus = new XiaoliManus(allTools, dashscopeChatModel);
+        return xiaoliManus.runStream(message);
+    }
+
+}
